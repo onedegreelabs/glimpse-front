d982590431b91a3766ca61430a3a5f6e219000ed
jodie9596@gmail.com <jodie9596@gmail.com>
2023-11-26 10:27:37 +0900

    Feat: footer 작업 및 전역 css 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import {Inter} from 'next/font/google';
 import './globals.css';
 import styles from './layout.module.scss';
+import Footer from '@/components/Footer/page';
 
 const inter = Inter({subsets: ['latin']});
 export default function RootLayout({children}: {children: React.ReactNode}) {
@@ -8,6 +9,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en">
       <body className={inter.className}>
         <div className={styles['children-wrapper']}>{children}</div>
+        <Footer />
       </body>
     </html>
   );
--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -2,12 +2,9 @@
 import styles from './page.module.scss';
 import clsx from 'clsx';
 export default function Footer() {
-  const a = true;
   return (
     <div className={styles['footer-wrapper']}>
-      <div className={clsx(styles['text-area'], a ? styles['a'] : styles['b'])}>
-        hi
-      </div>
+      <div className={clsx(styles['text-area'])}>Glimpse is made by XZY</div>
     </div>
   );
 }
