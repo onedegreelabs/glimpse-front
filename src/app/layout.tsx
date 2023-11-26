7b5c9577dd427354156641307b848b10fda62c64
jodie9596@gmail.com <jodie9596@gmail.com>
2023-11-26 09:38:14 +0900

    Feat: 불필요 라이브러리 삭제, 프로젝트 내에서 파일 개행 디폴트 값 LF설정 및 crlf도 git에 add 할 시에 자동으로 lf 변경 설정, 프로젝트에서 module.scss파일 불러올 수 있도록 타입 선언

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import {Inter} from 'next/font/google';
-import classNames from 'classnames';
 import './globals.css';
 import styles from './layout.module.scss';
 
 const inter = Inter({subsets: ['latin']});
-const cn = classNames.bind(styles);
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
