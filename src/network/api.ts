9e4f2da578b37fd861fa36afd9be51b6cfc8c8b8
unknown <jodie9596@gmail.com>
2023-12-07 22:03:53 +0900

    Feat: api파일 수정

--- src/network/api.ts
+++ src/network/api.ts
@@ -1,24 +1,14 @@
 import {axiosInstance} from './headers';
 
 export const sendMailWithCode = async (email: string) => {
-  try {
-    const response = await axiosInstance.post('/mails/code/', {
-      email,
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to send mail with code');
-  }
+  return await axiosInstance.post('/mails/code/', {
+    email,
+  });
 };
 
 export const verifyEmailCode = async (email: string, code: string) => {
-  try {
-    const response = await axiosInstance.post('/auth/email/', {
-      email,
-      code,
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to verify code');
-  }
+  return await axiosInstance.post('/auth/email/', {
+    email,
+    code,
+  });
 };
