393ee4b4a1885b8d7501aa47ed08b8d9b50d2d82
unknown <jodie9596@gmail.com>
2023-12-13 22:30:07 +0900

    Feat: 변경된 api 주소 적용 및 event-create api 바인딩 작업 진행중

--- src/network/api.ts
+++ src/network/api.ts
@@ -1,14 +1,56 @@
 import {axiosInstance} from './headers';
 
 export const sendMailWithCode = async (email: string) => {
-  return await axiosInstance.post('/mails/code/', {
+  return await axiosInstance.post('mails/code/', {
     email,
   });
 };
 
 export const verifyEmailCode = async (email: string, code: string) => {
-  return await axiosInstance.post('/auth/email/', {
+  return await axiosInstance.post('auth/email/', {
     email,
     code,
+  });
+};
+
+interface CreateEventType {
+  organizationId: number;
+  title: string;
+  type: string;
+  visibility: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  link: string;
+  handle: string;
+  description: string;
+  tags: string[];
+}
+export const createEvent = async (params: CreateEventType) => {
+  const {
+    organizationId,
+    title,
+    type,
+    visibility,
+    startDate,
+    endDate,
+    location,
+    link,
+    handle,
+    description,
+    tags,
+  } = params;
+  return await axiosInstance.post('events', {
+    organizationId,
+    title,
+    type,
+    visibility,
+    startDate,
+    endDate,
+    location,
+    link,
+    handle,
+    description,
+    tags,
   });
 };
