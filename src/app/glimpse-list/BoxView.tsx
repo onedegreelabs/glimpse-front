9d2935b0c5fb0e34fdbe62706a97bcfc1ffd50f1
monii <moniyel@gmail.com>
2024-01-08 13:51:37 +0900

    Feat: 글림스페이지 other 유저 페이지 링크연결

--- src/app/glimpse-list/BoxView.tsx
+++ src/app/glimpse-list/BoxView.tsx
@@ -4,6 +4,7 @@ import styles from './boxView.module.scss';
 import Card from '@/components/Card/page';
 import {Glimpse} from './mock/glimpses';
 import IconText from '@/components/IconText/page';
+import Link from 'next/link';
 
 interface BoxProps {
   glimpses: Glimpse[];
@@ -12,69 +13,71 @@ interface BoxProps {
 export default function BoxView({glimpses}: BoxProps) {
   return (
     <>
-      {glimpses.map(data => (
-        <Card key={data.id}>
-          <div className={styles['glimpse-list-wrapper']}>
-            <div className={styles['card-header']}>
-              <img src="/assets/glimpse-list/bookmark-icon.svg" />
-            </div>
-            <div className={styles['profile-wrapper']}>
-              <div>
-                <IconText
-                  src={'/assets/glimpse-list/location-icon.svg'}
-                  alt={'위치 아이콘'}
-                  width={24}
-                  height={24}
-                  text={'Seoul, Korea'}
-                />
-                <p className={styles['profile-name']}>{data.name}</p>
-                <div className={styles['position-wrapper']}>
-                  {data.position.map((d: string, index: number) => (
-                    <Chip
-                      key={index}
-                      label={d}
-                      height={28}
-                      backgroundColor={index === 0 ? '#C1AEF6' : '#F3F3F3'}
-                      borderRadius={4}
-                    />
-                  ))}
-                </div>
+      {glimpses.map((data, index) => (
+        <Link key={`profile-card-${index}`} href={'/users/3'}>
+          <Card key={data.id}>
+            <div className={styles['glimpse-list-wrapper']}>
+              <div className={styles['card-header']}>
+                <img src="/assets/glimpse-list/bookmark-icon.svg" />
               </div>
-              <Avatar
-                src="/assets/glimpse-list/temp-glimpse-list-img.jpg"
-                alt="프로필이미지"
-                height={70}
-                width={70}
-              />
-            </div>
-            <div>
-              <p>{data.text}</p>
-            </div>
-            <div className={styles['hobby-wrapper']}>
-              {data.hobby.map((d: string, index: number) => (
-                <Chip
-                  key={`hobby-${index}`}
-                  label={`#${d}`}
-                  height={24}
-                  backgroundColor="#F3F3F3"
-                  borderRadius={30}
+              <div className={styles['profile-wrapper']}>
+                <div>
+                  <IconText
+                    src={'/assets/glimpse-list/location-icon.svg'}
+                    alt={'위치 아이콘'}
+                    width={24}
+                    height={24}
+                    text={'Seoul, Korea'}
+                  />
+                  <p className={styles['profile-name']}>{data.name}</p>
+                  <div className={styles['position-wrapper']}>
+                    {data.position.map((d: string, index: number) => (
+                      <Chip
+                        key={index}
+                        label={d}
+                        height={28}
+                        backgroundColor={index === 0 ? '#C1AEF6' : '#F3F3F3'}
+                        borderRadius={4}
+                      />
+                    ))}
+                  </div>
+                </div>
+                <Avatar
+                  src="/assets/glimpse-list/temp-glimpse-list-img.jpg"
+                  alt="프로필이미지"
+                  height={70}
+                  width={70}
                 />
-              ))}
+              </div>
+              <div>
+                <p>{data.text}</p>
+              </div>
+              <div className={styles['hobby-wrapper']}>
+                {data.hobby.map((d: string, index: number) => (
+                  <Chip
+                    key={`hobby-${index}`}
+                    label={`#${d}`}
+                    height={24}
+                    backgroundColor="#F3F3F3"
+                    borderRadius={30}
+                  />
+                ))}
+              </div>
+              <div className={styles['link-wrapper']}>
+                {data.link.map((_, index: number) => (
+                  <div
+                    key={`link_${index}`}
+                    style={{
+                      width: '32px',
+                      height: '32px',
+                      backgroundColor: '#F3F3F3',
+                    }}
+                  />
+                ))}
+              </div>
             </div>
-            <div className={styles['link-wrapper']}>
-              {data.link.map((_, index: number) => (
-                <div
-                  key={`link_${index}`}
-                  style={{
-                    width: '32px',
-                    height: '32px',
-                    backgroundColor: '#F3F3F3',
-                  }}
-                />
-              ))}
-            </div>
-          </div>
-        </Card>
+          </Card>
+        </Link>
       ))}
     </>
   );
--- src/app/glimpse-list/GridView.tsx
+++ src/app/glimpse-list/GridView.tsx
@@ -3,6 +3,7 @@ import Chip from '@/components/Chip/page';
 import styles from './gridView.module.scss';
 import Card from '@/components/Card/page';
 import {Glimpse} from './mock/glimpses';
+import Link from 'next/link';
 
 // NOTE: list props로 변경하기
 
@@ -14,45 +15,47 @@ export default function GridView({glimpses}: GridViewProps) {
   return (
     <div className={styles['glimpse-grid-container']}>
       {glimpses.map((data, index) => (
-        <Card key={index}>
-          <div className={styles['glimpse-grid-wrapper']}>
-            <div className={styles['card-header']}>
-              <img src="/assets/glimpse-list/bookmark-icon.svg" />
-            </div>
-            <div className={styles['profile-wrapper']}>
-              <Avatar
-                src="/assets/glimpse-list/temp-glimpse-list-img.jpg"
-                alt="프로필이미지"
-                height={40}
-                width={40}
-              />
-              <p className={styles['profile-name']}>{data.name}</p>
-            </div>
-            <div className={styles['position-wrapper']}>
-              {data.position.map((d: string, index: number) => (
-                <Chip
-                  key={index}
-                  label={d}
-                  height={28}
-                  backgroundColor={index === 0 ? '#C1AEF6' : '#F3F3F3'}
-                  borderRadius={4}
+        <Link key={`profile-card-${index}`} href={'/users/3'}>
+          <Card key={index}>
+            <div className={styles['glimpse-grid-wrapper']}>
+              <div className={styles['card-header']}>
+                <img src="/assets/glimpse-list/bookmark-icon.svg" />
+              </div>
+              <div className={styles['profile-wrapper']}>
+                <Avatar
+                  src="/assets/glimpse-list/temp-glimpse-list-img.jpg"
+                  alt="프로필이미지"
+                  height={40}
+                  width={40}
                 />
-              ))}
+                <p className={styles['profile-name']}>{data.name}</p>
+              </div>
+              <div className={styles['position-wrapper']}>
+                {data.position.map((d: string, index: number) => (
+                  <Chip
+                    key={index}
+                    label={d}
+                    height={28}
+                    backgroundColor={index === 0 ? '#C1AEF6' : '#F3F3F3'}
+                    borderRadius={4}
+                  />
+                ))}
+              </div>
+              <div className={styles['link-wrapper']}>
+                {data.link.map((_, index: number) => (
+                  <div
+                    key={`link_${index}`}
+                    style={{
+                      width: '32px',
+                      height: '32px',
+                      backgroundColor: '#F3F3F3',
+                    }}
+                  />
+                ))}
+              </div>
             </div>
-            <div className={styles['link-wrapper']}>
-              {data.link.map((_, index: number) => (
-                <div
-                  key={`link_${index}`}
-                  style={{
-                    width: '32px',
-                    height: '32px',
-                    backgroundColor: '#F3F3F3',
-                  }}
-                />
-              ))}
-            </div>
-          </div>
-        </Card>
+          </Card>
+        </Link>
       ))}
     </div>
   );
