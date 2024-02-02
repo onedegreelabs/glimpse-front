5b2ad91e556f0ab53541562de798924337e7944c
monii <moniyel@gmail.com>
2024-02-02 16:55:35 +0900

    refact: 마이 프로필 페이지 리팩토링 - about me 섹션 컴포넌트로 분리

---
+++ src/containers/my/profile/sections/AboutMe.tsx
@@ -0,0 +1,47 @@
+import styles from './aboutMe.module.scss';
+import {useEffect, useState} from 'react';
+
+import Card from '@/components/Card/page';
+import {IProfileCard} from '@/types/profileType';
+import getCardsByType from '@/utils/getCardsByType';
+import {ABOUTME} from '../constans/profile';
+
+interface AboutMeCardProps {
+  cards: IProfileCard[];
+}
+
+export default function AboutMeCard({cards}: AboutMeCardProps) {
+  const [aboutMe, setAboutMe] = useState<IProfileCard>({
+    id: 0,
+    type: 'ABOUTME',
+    content: ['UI테스트중'],
+    isVisible: true,
+    color: '#FFFFFF',
+  });
+
+  useEffect(() => {
+    if (cards !== undefined) {
+      const aboutMeCard = getCardsByType(cards, ABOUTME);
+      setAboutMe(aboutMeCard[0]);
+    }
+  }, [cards]);
+
+  return (
+    <>
+      {aboutMe && aboutMe.content.length > 0 && (
+        <>
+          <div className={styles['title']}>
+            <span>About me</span>
+          </div>
+          <div className={styles['content-wrapper']}>
+            <Card height={168}>
+              <div className={styles['content']}>
+                <p>{aboutMe.content || ''}</p>
+              </div>
+            </Card>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
