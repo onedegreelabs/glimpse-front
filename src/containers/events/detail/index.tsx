2dddb115bdef94b6e504d60b698efe5e595a1e6a
unknown <jodie9596@gmail.com>
2024-02-19 16:26:27 +0900

    refact: index 파일 style 제거

--- src/containers/events/detail/index.tsx
+++ src/containers/events/detail/index.tsx
@@ -1,4 +1,3 @@
-import styles from './index.module.scss';
 import EventInfo from './sections/EventInfo';
 import MiddleControlSection from './sections/MiddleControlSection';
 import UserList from './sections/UserList';
@@ -6,11 +5,11 @@ import TopBanner from './sections/TopBanner';
 
 export default function EventDetailContainer() {
   return (
-    <div className={styles['events-detail-wrapper']}>
+    <>
       <TopBanner />
       <EventInfo />
       <MiddleControlSection />
       <UserList />
-    </div>
+    </>
   );
 }
