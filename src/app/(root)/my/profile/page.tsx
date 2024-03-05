9ec0f4b8a855b2c98a92f0e42e7726b83e3c94c2
김영훈 <joseph0926@naver.com>
2024-03-05 09:29:55 +0900

    import 경로 잘못된 부분 수정

--- src/app/(root)/my/profile/page.tsx
+++ src/app/(root)/my/profile/page.tsx
@@ -1,4 +1,4 @@
-import Container from '@/components/Container/Container';
+import Container from '@/components/container/Container';
 import MyProfileContainer from '@/containers/my/profile';
 import React from 'react';
 
