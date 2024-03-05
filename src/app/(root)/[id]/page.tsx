bd81dba8f0187ad5b4d3de65c8dc023ecddebad9
unknown <jodie9596@gmail.com>
2024-03-05 10:38:30 +0900

    refact: container 폴더 경로 수정

--- src/app/(root)/[id]/page.tsx
+++ src/app/(root)/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Container from '@/components/container/Container';
+import Container from '@/components/Container/Container';
 import ProfilesContainer from '@/containers/profiles';
 import React from 'react';
 
