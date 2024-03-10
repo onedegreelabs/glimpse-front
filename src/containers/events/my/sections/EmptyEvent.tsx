7c7d911cf3604ec5251703e498b3317208929570
jodie9596@gmail.com <jodie9596@gmail.com>
2024-03-10 19:45:56 +0900

    feat: img import path modify

--- src/containers/events/my/sections/EmptyEvent.tsx
+++ src/containers/events/my/sections/EmptyEvent.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
 import styles from './emptyEvent.module.scss';
-import fileIcon from '@/../public/icons/file_icon.svg';
-import plusIcon from '@/../public/icons/plus2_icon.svg';
 
 export default function EmptyEvent() {
   return (
     <div className={styles['empty-event-wrapper']}>
       <div className={styles['content-wrapper']}>
         <div className={styles['img-wrapper']}>
-          <Image src={fileIcon} alt="file-icon" />
+          <Image
+            src={'/icons/file_icon.svg'}
+            alt="file-icon"
+            width={168}
+            height={168}
+          />
         </div>
         <div className={styles['main-title']}>
           {"You don't have any events"}
@@ -17,7 +20,12 @@ export default function EmptyEvent() {
           {'Would you like to create one?'}
         </div>
         <div className={styles['create-button']}>
-          <Image src={plusIcon} alt="plus-icon" width={20} height={20} />
+          <Image
+            src={'/icons/plus2_icon.svg'}
+            alt="plus-icon"
+            width={20}
+            height={20}
+          />
           <div className={styles['button-text']}>Create Event</div>
         </div>
       </div>
