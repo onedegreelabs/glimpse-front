63213af5ad70031a6f987b6279cb3f779185f52f
jodie9596@gmail.com <jodie9596@gmail.com>
2024-03-11 21:51:41 +0900

    refact: swr 폴더 구조 변경

---
+++ src/events/getMyEventList.ts
@@ -0,0 +1,15 @@
+import {customAxios} from '@/services/headers';
+import useSWR from 'swr';
+export default function useMyEventList(count: number) {
+  const fetcher = async function () {
+    const res = await customAxios.get(`events/my-events?take=${count}`);
+    return res.data;
+  };
+
+  const {data, error, isLoading} = useSWR(
+    `events/my-events?take=${count}`,
+    fetcher
+  );
+
+  return {data, error, isLoading};
+}
