48b97113a0b14037a0542d759b507e1d98d81da9
monii <moniyel@gmail.com>
2024-03-24 23:42:09 +0900

    feat: footer ui 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import {Inter} from 'next/font/google';
 import './globals.css';
 import styles from './layout.module.scss';
+import Header from '@/components/header/Header';
+import Footer from '@/components/footer/Footer';
 
 const inter = Inter({subsets: ['latin']});
 export default function RootLayout({children}: {children: React.ReactNode}) {
@@ -8,6 +10,8 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en">
       <body className={inter.className}>
         <div className={styles['children-wrapper']}>{children}</div>
+        <div className={styles['children-wrapper']}>{children}</div>
+        <Footer />
       </body>
     </html>
   );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,54 @@
 'use client';
+
 import styles from './footer.module.scss';
 import clsx from 'clsx';
+import Link from 'next/link';
+import Image from 'next/image';
+import {usePathname} from 'next/navigation';
+import {useEffect, useState} from 'react';
+
+interface Footer {
+  url: string;
+  name: string;
+  icon: string;
+}
+
+const FOOTER_LINK: Footer[] = [
+  {url: '/', name: 'Home', icon: 'home'},
+  {url: '/discover', name: 'Discover', icon: 'discover'},
+  {url: '/events/my', name: 'My Events', icon: 'calendar'},
+  {url: '/my/profile', name: 'My Profile', icon: 'contacts'},
+];
+
 export default function Footer() {
+  const pathname = usePathname();
+  const [isCurrentPage, setIsCurrentPage] = useState('/my/profile');
+
+  useEffect(() => {
+    setIsCurrentPage(pathname ?? '/my/profile');
+  }, [pathname]);
+
   return (
-    <div className={styles['footer-wrapper']}>
-      <div className={clsx(styles['text-area'])}>Glimpse is made by XZY</div>
-    </div>
+    <footer className={styles['footer']}>
+      <ul className={styles['footer-container']}>
+        {FOOTER_LINK.map(link => (
+          <Link key={link.name} href={link.url}>
+            <li className={styles['link-wrapper']}>
+              <div className={styles['icon-wrapper']}>
+                <Image
+                  src={`/assets/footer/${
+                    isCurrentPage === link.url ? 'solid' : 'outline'
+                  }-${link.icon}.svg`}
+                  alt={link.name}
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className={styles['icon-name']}>{link.name}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </footer>
   );
 }
