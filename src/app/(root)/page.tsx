6341972f232538116ec653ce6e556d293a0f3712
unknown <jodie9596@gmail.com>
2024-03-25 14:22:54 +0900

    첫페이지 event discover 설정

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+import {useRouter} from 'next/navigation';
+import {useEffect} from 'react';
+
 export default function HomePage() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/events/discover');
+  }, []);
   return <div>메인페이지를 지정해주세요.</div>;
 }
