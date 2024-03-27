05c980c2300a1d0a882a997c06e62d115dd4c512
monii <moniyel@gmail.com>
2024-03-27 19:43:06 +0900

    fix: 임시 footer url 링크 변경 (#72)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import {Inter} from 'next/font/google';
 import './globals.css';
 import styles from './layout.module.scss';
-import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 
 const inter = Inter({subsets: ['latin']});
@@ -9,7 +8,6 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className={styles['children-wrapper']}>{children}</div>
         <div className={styles['children-wrapper']}>{children}</div>
         <Footer />
       </body>
