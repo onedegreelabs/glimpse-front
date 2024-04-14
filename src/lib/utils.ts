65b77a8dd32777be25de5a644ef09e8a486016f8
jodie9596@gmail.com <jodie9596@gmail.com>
2024-04-14 15:21:19 +0900

    [GLIM-21] 유틸 파일 통합

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,3 +21,21 @@ export function getTimeTextFromDateObj(dateObj: Date) {
 
   return `${hours}:${minutes} ${kstDdateObj.getHours() >= 12 ? 'PM' : 'AM'}`;
 }
+
+export function getQueryString(url: string): {[key: string]: string} {
+  const queryStringIndex = url.indexOf('?');
+  if (queryStringIndex === -1) {
+    return {}; // 쿼리스트링이 없는 경우 빈 객체 반환
+  }
+
+  const queryString = url.substring(queryStringIndex + 1);
+  const queryParams: {[key: string]: string} = {};
+  const pairs = queryString.split('&');
+
+  for (const pair of pairs) {
+    const [key, value] = pair.split('=');
+    queryParams[decodeURIComponent(key)] = decodeURIComponent(value || '');
+  }
+
+  return queryParams;
+}
