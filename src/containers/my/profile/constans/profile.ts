725851ba42d51ffdbb1433d28b86ad874bfcf7c8
panseung <jodie9596@gmail.com>
2024-04-15 10:17:14 +0900

    [GLIM-21] 불필요 변수 삭제

--- src/containers/my/profile/constans/profile.ts
+++ src/containers/my/profile/constans/profile.ts
@@ -1,51 +1,5 @@
 import {TProfile, UpdateUserDtoRequest} from '@/types/profileType';
 
-export const linkImg = [
-  {
-    alt: 'github',
-    src: '/assets/favicon/github.svg',
-  },
-  {
-    alt: 'facebook',
-    src: '/assets/favicon/facebook.jpg',
-  },
-  {
-    alt: 'dribble',
-    src: '/assets/favicon/dribble.jpg',
-  },
-  {
-    alt: 'instagram',
-    src: '/assets/favicon/instagram.svg',
-  },
-  {
-    alt: 'linkedin',
-    src: '/assets/favicon/linkedin.jpg',
-  },
-  {
-    alt: 'medium',
-    src: '/assets/favicon/medium.jpg',
-  },
-];
-
-export const INTROTITLE = 'INTROTITLE';
-export const INTROCAREER = 'INTROCAREER';
-export const ABOUTME = 'ABOUTME';
-export const LINK = 'LINK';
-export const HASHTAG = 'HASHTAG';
-
-export const DEPARTMENT = [
-  'Engineering',
-  'Product',
-  'Business',
-  'Marketing',
-  'Finance',
-  'Developer Relation',
-  'Customer Experience',
-  'Operation',
-  'Design',
-  'Other',
-];
-
 export const initialProfile: TProfile = {
   createdAt: '',
   updatedAt: '',
