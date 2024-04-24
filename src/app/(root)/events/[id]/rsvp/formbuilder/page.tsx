0c28c9f2aa510f25a5e6cfbe5e1b4e01eebd152c
JaeYong0107 <dwd07@naver.com>
2024-04-24 18:09:15 +0900

    [GLIM-36] Guest registraion 퍼블리싱 및 구조 조정

--- src/app/(root)/events/[id]/rsvp/formbuilder/page.tsx
+++ src/app/(root)/events/[id]/rsvp/formbuilder/page.tsx
@@ -1,5 +1,22 @@
-import RsvpFormBuilder from './sections/RsvpFormBuilder';
+'use client';
+import {useState} from 'react';
+
+import RsvpFormBuilder from './sections/RsvpFormBuilder/RsvpFormBuilder';
+import PendingApproval from './sections/PendingApproval/PendingApproval';
+import Approved from './sections/Approved/Approved';
+import Rejected from './sections/Rejected/Rejected';
+
+type PageType = 'default' | 'pending' | 'approve' | 'reject';
 
 export default function RsvpFormBuilderPage() {
-  return <RsvpFormBuilder />;
+  const [page, setPage] = useState<PageType>('default');
+
+  return (
+    <>
+      {page === 'default' && <RsvpFormBuilder setPage={setPage} />}
+      {page === 'pending' && <PendingApproval />}
+      {page === 'approve' && <Approved />}
+      {page === 'reject' && <Rejected />}
+    </>
+  );
 }
---
+++ src/app/(root)/events/[id]/rsvp/formbuilder/sections/RsvpFormBuilder/RsvpFormBuilder.tsx
@@ -0,0 +1,201 @@
+'use client';
+import {useState} from 'react';
+import Image from 'next/image';
+
+import styles from './RsvpFormBulder.module.scss';
+import CustomQuestionModal from '../../components/CustomQuestionModal/CustomQuestionModal';
+
+type PageType = 'default' | 'pending' | 'approve' | 'reject';
+
+interface Props {
+  setPage: React.Dispatch<React.SetStateAction<PageType>>;
+}
+
+export default function RsvpFormBuilder({setPage}: Props) {
+  const [showModal, setShowModal] = useState(false);
+  const [toggle, setToggle] = useState(false);
+
+  function closeModal() {
+    setShowModal(false);
+  }
+
+  return (
+    <div className={styles['builder-container']}>
+      <div className={styles['quest-list']}>
+        <div>Guest list</div>
+        <div className={styles['purple-font']}>RSVP Form Builder</div>
+      </div>
+      <div className={styles['container']}>
+        <div className={styles['approval-required']}>
+          <div>Approval required</div>
+          <div
+            className={`${styles['toggle-button']} ${
+              toggle ? styles['active'] : ''
+            }`}
+            onClick={() => setToggle(prev => !prev)}
+          >
+            <div
+              className={`${styles['toggle']} ${
+                toggle ? styles['active'] : ''
+              }`}
+            />
+          </div>
+        </div>
+        <p>
+          {`Guest will be automatically approved after form submission when
+        'Approval required off'`}
+        </p>
+        <div className={styles['guest-registration']}>
+          <div>Guest registration</div>
+          <ul>
+            <li>
+              <div
+                className={styles['pending-approval-card']}
+                onClick={() => setPage('pending')}
+              >
+                0
+                <span className={styles['pending-approval']}>
+                  <Image
+                    src={'/assets/events/rsvp/pending.svg'}
+                    alt="pending"
+                    width={20}
+                    height={20}
+                  />
+                  Pending approval
+                </span>
+              </div>
+            </li>
+            <li>
+              <div
+                className={styles['pending-approval-card']}
+                onClick={() => setPage('approve')}
+              >
+                0
+                <div className={styles['pending-approval']}>
+                  <Image
+                    src={'/assets/events/rsvp/approved.svg'}
+                    alt="approved"
+                    width={20}
+                    height={20}
+                  />
+                  Approved
+                </div>
+              </div>
+            </li>
+            <li>
+              <div
+                className={styles['pending-approval-card']}
+                onClick={() => setPage('reject')}
+              >
+                0
+                <div className={styles['pending-approval']}>
+                  <Image
+                    src={'/assets/events/rsvp/rejected.svg'}
+                    alt="rejected"
+                    width={20}
+                    height={20}
+                  />
+                  Rejected
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className={styles['registration-question']}>
+          <div>Registration Questions</div>
+          <div className={styles['profile-question']}>Profile Questions</div>
+          <ol>
+            <li>
+              Full Name<div className={styles['necessary']}>Necessary</div>
+            </li>
+            <li>
+              Nickname/Preferred Name
+              <div className={styles['necessary']}>Necessary</div>
+            </li>
+            <li>
+              Email (registered with Glimpse)
+              <div className={styles['necessary']}>Necessary</div>
+            </li>
+            <li>
+              Purpose of Participation
+              <div className={styles['necessary']}>Necessary</div>
+            </li>
+            <li>
+              Location
+              <div
+                className={`${styles['toggle-button']} ${
+                  toggle ? styles['active'] : ''
+                }`}
+                onClick={() => setToggle(prev => !prev)}
+              >
+                <div
+                  className={`${styles['toggle']} ${
+                    toggle ? styles['active'] : ''
+                  }`}
+                />
+              </div>
+            </li>
+            <li>
+              Specialization
+              <div
+                className={`${styles['toggle-button']} ${
+                  toggle ? styles['active'] : ''
+                }`}
+                onClick={() => setToggle(prev => !prev)}
+              >
+                <div
+                  className={`${styles['toggle']} ${
+                    toggle ? styles['active'] : ''
+                  }`}
+                />
+              </div>
+            </li>
+            <li>
+              Company
+              <div
+                className={`${styles['toggle-button']} ${
+                  toggle ? styles['active'] : ''
+                }`}
+                onClick={() => setToggle(prev => !prev)}
+              >
+                <div
+                  className={`${styles['toggle']} ${
+                    toggle ? styles['active'] : ''
+                  }`}
+                />
+              </div>
+            </li>
+            <li>
+              Interest (tag)
+              <div
+                className={`${styles['toggle-button']} ${
+                  toggle ? styles['active'] : ''
+                }`}
+                onClick={() => setToggle(prev => !prev)}
+              >
+                <div
+                  className={`${styles['toggle']} ${
+                    toggle ? styles['active'] : ''
+                  }`}
+                />
+              </div>
+            </li>
+          </ol>
+          <div className={styles['custom-question']}>
+            <div>Custom Question</div>
+            <button onClick={() => setShowModal(true)}>
+              <Image
+                src={'/assets/events/rsvp/plus.svg'}
+                alt="plus"
+                width={26.67}
+                height={26.67}
+              />
+              Add Question
+            </button>
+          </div>
+        </div>
+      </div>
+      {showModal && <CustomQuestionModal onClose={closeModal} />}
+    </div>
+  );
+}
