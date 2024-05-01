098bdcc8b165b73ef63af6f8f0cdc39ad78c46a4
JaeYong0107 <dwd07@naver.com>
2024-05-01 23:44:04 +0900

    [GLIM-105] isHost에 따라 rsvp버튼 조정

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -3,7 +3,11 @@ import UserList from './sections/UserList';
 import TopBanner from './sections/TopBanner';
 import EventInfo from './sections/EventInfo';
 import styles from './page.module.scss';
-import {useEventDetail, useEventUser} from '@/hooks/swr/useEvents';
+import {
+  useEventDetail,
+  useEventUser,
+  useMyEventList,
+} from '@/hooks/swr/useEvents';
 import {usePathname} from 'next/navigation';
 import {useEffect, useState} from 'react';
 export default function EventDetailPage() {
@@ -12,6 +16,14 @@ export default function EventDetailPage() {
   const eventHandle = pathnameList?.[pathnameList.length - 1];
   const {data} = useEventDetail(eventHandle);
   const [eventId, setEventId] = useState(0);
+  const myEvents = useMyEventList(100); // // host 판별 임시 API(API 나오기 전)
+
+  // host 판별 임시 로직 (API 나오기 전)
+  const isHost = myEvents.data?.data?.find(
+    (item: {handle: string}) => item.handle === eventHandle
+  );
+  console.log(myEvents);
+  console.log(isHost);
 
   useEffect(() => {
     if (data?.data?.id) {
@@ -29,7 +41,7 @@ export default function EventDetailPage() {
         coverImage={data?.data?.coverImage}
       />
       <EventInfo eventDetailData={data?.data} />
-      <UserList eventUserData={eventUserData?.data} />
+      <UserList eventUserData={eventUserData?.data} isHost={isHost} />
     </div>
   );
 }
