f8880a0d3620bc1b2b46e6ed7e3fa79f2f8b8add
seungHwan0517 <seunghwan.kim@susimdal.com>
2024-05-24 20:19:08 +0900

    새 프로필 페이지 퍼블리싱

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -17,145 +17,208 @@ export default function ProfilePage() {
   const userProfile = useUserProfile(userId);
 
   // profile 정보 할당
-  const [lastName, setLastName] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [introduction, setIntroduction] = useState('');
-  const [role, setRole] = useState('');
-  const [department, setDepartment] = useState('');
-  const [region, setRegion] = useState('');
-  const [snsList, setSnsList] = useState<SnsType[]>();
-  const [profileCardList, setProfileCardList] = useState<ProfileCardType[]>([]);
-  const [userTag, setUserTag] = useState<string[]>([]);
-  const [profileImage, setProfileImage] = useState();
 
   useEffect(() => {
     if (userProfile?.data?.data) {
-      const profileData = userProfile.data.data;
-      const {
-        familyName,
-        givenName,
-        introduction,
-        profileCard,
-        region,
-        role,
-        sns,
-        userTag,
-      } = profileData;
-      setLastName(givenName);
-      setFirstName(familyName);
-      setIntroduction(introduction);
-      setRole(role);
-      setProfileCardList(profileCard);
-      setRegion(region);
-      setSnsList(sns);
-      setUserTag(userTag);
+      return;
     }
   }, [userProfile]);
 
+  const TAB_ITEM_LIST = ['Profile', 'Connection', 'Activity'];
+  const [tabList, setTabList] = useState([true, false, false]);
+  const changeActiveTab = (idx: number) => {
+    const newTabList = [false, false, false];
+    newTabList[idx] = true;
+    setTabList(() => newTabList);
+  };
+
   return (
     <div className={styles['my-profile-wrapper']}>
-      <div className={styles['profile-image-wrapper']}>
-        {profileImage ? <div /> : <div className={styles['empty-image']} />}
-        <div className={styles['add-image-btn']}>
-          <Image
-            src="/icons/picture.svg"
-            width={16}
-            height={12}
-            alt="picutre"
-          />
+      <div className={styles['card-wrapper']}>
+        <div className={styles['card']}>
+          <div className={styles['name-wrapper']}>
+            <p className={styles['name-text']}>Sarah</p>
+            <p className={styles['name-text']}>Kim</p>
+          </div>
+          <p className={styles['role-text']}>Product Manager @ XYZ</p>
         </div>
-      </div>
-      <div className={styles['name-area']}>
-        <input
-          maxLength={10}
-          placeholder="Last Name"
-          value={lastName}
-          onChange={e => {
-            setLastName(e.target.value);
-          }}
-        />
-        <input
-          maxLength={10}
-          placeholder="First Name"
-          value={firstName}
-          onChange={e => {
-            setFirstName(e.target.value);
-          }}
-        />
-      </div>
-      <div className={styles['simple-introduce']}>
-        <input
-          placeholder="add bio..."
-          value={introduction}
-          onChange={e => {
-            setIntroduction(e.target.value);
-          }}
-        />
-      </div>
-      <div className={styles['career-area-wrapper']}>
-        <select>
-          <option>department</option>
-        </select>
-        <div className={styles['devidor']} />
-        <input
-          placeholder="company"
-          maxLength={15}
-          value={role}
-          onChange={e => {
-            setRole(e.target.value);
-          }}
-        />
-      </div>
-      <div className={styles['region-area']}>
-        <Image
-          src="/icons/Location.svg"
-          width={16}
-          height={16}
-          alt="location"
-        />
-        <input value={'Seoul, Korea'} onChange={() => {}} />
       </div>
 
-      <div className={styles['box-wrapper']}>
-        <div className={styles['title-text']}>Connect</div>
-        <div className={styles['sns-wrapper']}>
-          {snsList &&
-            snsList.map((snsData, idx) => {
-              return (
-                <Card height={64} key={idx}>
-                  <div className={styles['card-inner']}>
-                    <div className={styles['link-wrapper']}>
-                      <div className={styles['empty-link']} />
-                      {snsData.account}
-                    </div>
-                  </div>
-                </Card>
-              );
-            })}
+      <div className={styles['tab-wrapper']}>
+        {TAB_ITEM_LIST.map((tab, idx) => {
+          return (
+            <div
+              key={idx}
+              className={clsx(styles['tab-item'], {
+                [styles['active-tab']]: tabList[idx],
+              })}
+              onClick={() => {
+                changeActiveTab(idx);
+              }}
+            >
+              {TAB_ITEM_LIST[idx]}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className={styles['section-title']}>About me</div>
+      <div className={clsx(styles['gray-box'], styles['about-me-box'])}>
+        Passionate about the product cycle, I specialize in developing
+        strategies, managing complex projects/products, and leveraging data to
+        drive impactful business decisions.
+      </div>
+
+      <div className={styles['section-title']}>Social</div>
+      <div className={clsx(styles['gray-box'], styles['link-box'])}>
+        <Image
+          src={'/assets/sns-link/linkedin.jpg'}
+          width={32}
+          height={32}
+          alt="dirrble-icon"
+        />
+        <p>LinkdIn</p>
+      </div>
+      <div className={clsx(styles['gray-box'], styles['link-box'])}>
+        <Image
+          src={'/assets/sns-link/dribble.jpg'}
+          width={32}
+          height={32}
+          alt="dirrble-icon"
+        />
+        <p>Dribble</p>
+      </div>
+
+      <div className={styles['section-title']}>Experience</div>
+      <div className={clsx(styles['gray-box'], styles['work-box'])}>
+        <button className={styles['arrow-button']}>
+          <Image
+            src={'/icons/right_top_arrow.svg'}
+            width={14}
+            height={14}
+            alt="dirrble-icon"
+          />
+        </button>
+        <p className={styles['work-text']}>Work</p>
+        <hr />
+        <div className={styles['link-item']}>
+          <Image
+            src={'/assets/sns-link/github.jpg'}
+            width={32}
+            height={32}
+            alt="dirrble-icon"
+          />
+          <div className={styles['text-area']}>
+            <p className={styles['title-text']}>Information Process Analyst</p>
+            <p className={styles['sub-text']}>
+              Developed an onboarding automation flow that acted as a blueprint
+              for different departments
+            </p>
+          </div>
+        </div>
+        <div className={styles['link-item']}>
+          <Image
+            src={'/assets/sns-link/instagram.jpg'}
+            width={32}
+            height={32}
+            alt="dirrble-icon"
+          />
+          <div className={styles['text-area']}>
+            <p className={styles['title-text']}>Product Strategy </p>
+            <p className={styles['sub-text']}>
+              Analyzed KPIs, resulting in a 17% increase in user engagement
+              within three months
+            </p>
+          </div>
+        </div>
+        <div className={styles['link-item']}>
+          <Image
+            src={'/assets/sns-link/facebook.jpg'}
+            width={32}
+            height={32}
+            alt="dirrble-icon"
+          />
+          <div className={styles['text-area']}>
+            <p className={styles['title-text']}>It Intern</p>
+            <p className={styles['sub-text']}>
+              Developed and deployed AI solutions resulting in 23% increase in
+              email open rates
+            </p>
+          </div>
+        </div>
+        <div className={styles['link-item']}>
+          <Image
+            src={'/assets/sns-link/medium.jpg'}
+            width={32}
+            height={32}
+            alt="dirrble-icon"
+          />
+          <div className={styles['text-area']}>
+            <p className={styles['title-text']}>Business Development Intern</p>
+            <p className={styles['sub-text']}>
+              Conducted competitive research to help formulate the company’s
+              3-year strategy
+            </p>
+          </div>
         </div>
       </div>
-      <div className={styles['box-wrapper']}>
-        <div className={styles['title-text']}>Hashtag of interest</div>
-        <Card height={384}>
-          <div className={clsx([styles['card-inner'], styles['for-tag']])}>
-            <div className={styles['tag-item-wrapper']}>
-              {userTag.map((tag, i) => {
-                return (
-                  <div className={styles['tag-item']} key={i}>
-                    {tag}
-                    <Image
-                      src="/icons/delete.svg"
-                      width={24}
-                      height={24}
-                      alt="delete-button"
-                      className={styles['close-btn']}
-                    />
-                  </div>
-                );
-              })}
-            </div>
+
+      <div className={styles['section-title']}>Projects</div>
+      <div className={clsx(styles['gray-box'], styles['project-box'])}>
+        <button className={styles['arrow-button']}>
+          <Image
+            src={'/icons/right_top_arrow.svg'}
+            width={14}
+            height={14}
+            alt="dirrble-icon"
+          />
+        </button>
+        <p className={styles['work-text']}>Hackathon</p>
+        <hr />
+        <div className={styles['project-item']}>
+          <p className={styles['project-title']}>Project 1</p>
+          <div className={styles['circle-wrapper']}>
+            {[0, 0, 0].map((_, idx) => {
+              return <div key={idx} className={styles['round-circle']} />;
+            })}
           </div>
-        </Card>
+          <div className={styles['picture-wrapper']}>
+            <Image
+              src={'/test/example1.jpg'}
+              width={86}
+              height={56}
+              alt="dirrble-icon"
+            />
+            <Image
+              src={'/test/example2.jpg'}
+              width={86}
+              height={56}
+              alt="dirrble-icon"
+            />
+          </div>
+        </div>
+        <div className={styles['project-item']}>
+          <p className={styles['project-title']}>Project 2</p>
+          <div className={styles['circle-wrapper']}>
+            {[0, 0].map((_, idx) => {
+              return <div key={idx} className={styles['round-circle']} />;
+            })}
+          </div>
+          <div className={styles['picture-wrapper']}>
+            <Image
+              src={'/test/example3.jpg'}
+              width={86}
+              height={56}
+              alt="dirrble-icon"
+            />
+            <Image
+              src={'/test/example4.jpg'}
+              width={86}
+              height={56}
+              alt="dirrble-icon"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
